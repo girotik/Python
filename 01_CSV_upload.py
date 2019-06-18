8121133604150e4abea175d816adb73407806b8d
girotik <39525525+girotik@users.noreply.github.com>
2019-06-18 09:14:48 +0300

    Add files via upload

---
+++ 01_CSV_upload.py
@@ -0,0 +1,7 @@
+import pandas as pd
+pd.read_csv('/2019-06-07_ciancommercial.csv')
+df = pd.DataFrame('/2019-06-07_ciancommercial.csv')
+
+
+df_csv = pd.read_csv('/2019-06-07_ciancommercial.csv')
+df.head('/2019-06-07_ciancommercial.csv')   # Displays 1st Five Rows
